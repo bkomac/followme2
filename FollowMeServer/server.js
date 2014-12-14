15acda36dffceba6c9a4283e916e8324e3bb292d
bkomac <b@komac.si>
2014-12-14 18:44:02 +0100

    refactoring to socet.io

---
+++ FollowMeServer/server.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+	res.writeHead(200, {
+		'Content-Type' : 'text/plain'
+	});
+	res.end('This is socket.io endpoint.\n');
+});
+
+io.on('connection', function(socket) {
+	console.log("Conecting ...");
+	socket.on('position', function(msg) {
+		console.log("message... " + msg);
+		io.emit('position', msg);
+	});
+});
+http.listen(3000, function() {
+	console.log('listening on *:3000');
+});
