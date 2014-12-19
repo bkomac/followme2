8bd08ac529a587718fc4cbc853f66660f3f02578
bkomac <b@komac.si>
2014-12-19 20:54:30 +0100

    intorducing users sessions

--- FollowMeServer/server.js
+++ FollowMeServer/server.js
@@ -4,6 +4,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var sessions = [];
+var numOnlineUsers;
+
 console.log('Starting node on port ' + port + '...');
 
 app.get('/', function(req, res) {
@@ -21,6 +24,11 @@ io.on('connection', function(socket) {
 	socket.on('put_position', function(msg) {
 		// console.log(JSON.stringify(socket));
 		var pos = JSON.parse(msg);
+
+		msg.socketId = socket.id;
+
+		sessions.push(new User().setUser(msg.uddi, msg.socketId, msg.user));
+
 		console.log("user: " + pos.user + ":. #" + socket.id + "  " + pos.lat + " " + pos.lng);
 		io.emit('get_position', msg);
 	});
@@ -33,3 +41,32 @@ io.on('disconnect', function(socket) {
 http.listen(port, function() {
 	console.log('listening on:' + port + ' ...');
 });
+
+setInterval(function() {
+	for ( var user in sessions) {
+		if (user.tst < new Date().getTime() - 10 * 1000) {
+			console.log("Remove user..." + user.userName);
+			user = null;
+		}
+
+	}
+}, 10 * 1000);
+
+function User() {
+	this.id;
+	this.socketId;
+	this.userName;
+	this.tst;
+
+	this.setUser = function(id, socketId, userName) {
+		console.log("Adding user... " + userName);
+		this.id = id;
+		this.socketId = socketId;
+		this.userName = userName;
+		this.tst = new Date().getTime();
+		numOnlineUsers++;
+	};
+
+	
+
+}
