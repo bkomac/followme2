4ecc722865a3dd0a2805ac78c96890f34ee8551b
bobo <bobo@192.168.1.125>
2015-06-14 12:47:47 +0200

    count numOnline users added

--- FollowMeServer/server.js
+++ FollowMeServer/server.js
@@ -31,7 +31,7 @@ io.on('connection', function(socket) {
 		var pos = JSON.parse(msg);
 
 		pos.socketId = socket.id;
-		//pos.follows = numOnlineUsers;
+		pos.follows = numOnlineUsers;
 
 //		var user = new User();
 //		user.setUser(msg.uddi, msg.socketId, pos.user);
